bae5596ebb118dcf9ac8c6f41b4436467f14b79e
dazklein <lupardima@list.ru>
2021-05-05 23:39:53 +0300

    Добавьте файлы проекта.

---
+++ WaterDeliveryApp.Domain/ViewModel/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaterDeliveryApp.Domain
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
